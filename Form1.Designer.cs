a4463944c094ed4caf4268417b82e9f17cfdc6d5
Asher Abrams <asher813@aol.com>
2018-06-28 07:02:01 -0700

    Add files via upload
    Math quiz tutorial from MS Docs.  This commit is the initial form and time label..

---
+++ Form1.Designer.cs
@@ -0,0 +1,58 @@
+﻿namespace MathQuizTutorial01
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.timeLabel = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // timeLabel
+            // 
+            this.timeLabel.Location = new System.Drawing.Point(1526, 9);
+            this.timeLabel.Name = "timeLabel";
+            this.timeLabel.Size = new System.Drawing.Size(240, 60);
+            this.timeLabel.TabIndex = 0;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(16F, 31F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1778, 935);
+            this.Controls.Add(this.timeLabel);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label timeLabel;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,24 @@
+﻿// MathQuizTutorial01, Asher Abrams, 2018-06-28.
+// Tutorial from MS Docs.  Creates a timed math quiz.
+// Step 1:  I am modifying the size of the form from the specifications in the tutorial.
+
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MathQuizTutorial01
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+    }
+}
