35f28b43463ce0b99eea07f76b24fea286e8fcb5
Asher Abrams <asher813@aol.com>
2018-06-29 10:49:36 -0700

    Through Step 1.3.7.
    Created plusLeftLabel.

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -29,30 +29,58 @@
         private void InitializeComponent()
         {
             this.timeLabel = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.plusLeftLabel = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // timeLabel
             // 
+            this.timeLabel.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
             this.timeLabel.Location = new System.Drawing.Point(1526, 9);
             this.timeLabel.Name = "timeLabel";
             this.timeLabel.Size = new System.Drawing.Size(240, 60);
             this.timeLabel.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(1195, -2);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(282, 71);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Time Left";
+            // 
+            // plusLeftLabel
+            // 
+            this.plusLeftLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.plusLeftLabel.Location = new System.Drawing.Point(34, 9);
+            this.plusLeftLabel.Name = "plusLeftLabel";
+            this.plusLeftLabel.Size = new System.Drawing.Size(80, 100);
+            this.plusLeftLabel.TabIndex = 2;
+            this.plusLeftLabel.Text = "?";
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(16F, 31F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(1778, 935);
+            this.Controls.Add(this.plusLeftLabel);
+            this.Controls.Add(this.label1);
             this.Controls.Add(this.timeLabel);
             this.Name = "Form1";
             this.Text = "Form1";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
 
         private System.Windows.Forms.Label timeLabel;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label plusLeftLabel;
     }
 }
 
--- Form1.cs
+++ Form1.cs
@@ -1,6 +1,11 @@
 ﻿// MathQuizTutorial01, Asher Abrams, 2018-06-28.
 // Tutorial from MS Docs.  Creates a timed math quiz.
-// Step 1:  I am modifying the size of the form from the specifications in the tutorial.
+// Step 1:  I am modifying the size of the form from the specifications in the tutorial
+// because my machine has an unusually high pixel resolution and it won't display properly.
+// Step 1.2.1.  Label timeLabel is set to AutoSize=False because we need to leave the text empty
+// without the box borders shrinking to nothing (step 1.2.6).
+// Step 1.3.7.  We create the label plusLeftLabel, which will serve as a template.
+
 
 using System;
 using System.Collections.Generic;
